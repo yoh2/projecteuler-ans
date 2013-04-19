405de75fdd606744b6bc1d0cdfbb1c4367000678
yoh2 <yoh2.sdj@gmail.com>
2013-04-19 23:27:58 +0900

    Added an answer to ID 18.

---
+++ id_018.hs
@@ -0,0 +1,53 @@
+-- 何か命名規則がヘン……
+-- もうちょいマシな名前付けたい
+
+import Data.String (words)
+
+triangleStrings :: [String]
+triangleStrings = [
+               "75",
+              "95 64",
+             "17 47 82",
+            "18 35 87 10",
+           "20 04 82 47 65",
+          "19 01 23 75 03 34",
+         "88 02 77 73 07 63 67",
+        "99 65 04 28 06 16 70 92",
+       "41 41 26 56 83 40 80 70 33",
+      "41 48 72 33 47 32 37 16 94 29",
+     "53 71 44 65 25 43 91 52 97 51 14",
+    "70 11 33 28 77 73 17 78 39 68 17 57",
+   "91 71 52 38 17 14 91 43 58 50 27 29 48",
+  "63 66 04 68 89 53 67 30 73 16 69 87 40 31",
+ "04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"]
+
+triangleIntList :: [[Int]]
+triangleIntList = map ((map read) . words) triangleStrings
+
+-- (data, left, right)
+data BinTree a = Node (a, BinTree a, BinTree a) | Null
+
+toNode :: a -> BinTree a
+toNode x = Node (x, Null, Null)
+
+sharedLink :: [BinTree a] -> [BinTree a] -> [BinTree a]
+sharedLink xs [] = xs
+sharedLink (Node (x, _, r):xs) [y]        = Node (x, y, r) : xs
+sharedLink (Node (x, _, _):xs) (y0:y1:ys) = Node (x, y0, y1) : sharedLink xs (y1:ys)
+
+sharedLinkChain :: [[a]] -> [BinTree a]
+sharedLinkChain []       = []
+sharedLinkChain (xs:xss) = sharedLink (map toNode xs) (sharedLinkChain xss)
+
+triangleChain :: BinTree Int
+triangleChain = (sharedLinkChain triangleIntList) !! 0
+
+maxPath :: (Ord a, Num a) => BinTree a -> a
+maxPath Null             = 0
+maxPath (Node (x, r, l)) = x + max (maxPath r) (maxPath l)
+
+solution :: Int
+solution = maxPath triangleChain
+
+main :: IO ()
+main = print solution
