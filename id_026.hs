15911dec1ef75991f5f1882670f41fdd477c4d73
yoh2 <yoh2.sdj@gmail.com>
2013-05-03 04:11:34 +0900

    Added an answer to ID 26.

---
+++ id_026.hs
@@ -0,0 +1,25 @@
+import Data.List (findIndex, maximumBy)
+
+-- x: numerator
+-- y: denominator
+divCycleLength :: (Integral a) => a -> a -> Int
+divCycleLength x y = divCycleLengthSub x y []
+
+-- x: numerator
+-- y: denominator
+-- rs: 各桁をの除算をした時の余りリスト (逆順)
+divCycleLengthSub :: (Integral a) => a -> a -> [a] -> Int
+divCycleLengthSub 0 _ _  = 0
+divCycleLengthSub x y rs = case findIndex (==r) rs of
+                             Just n  -> n + 1
+                             Nothing -> divCycleLengthSub (r * 10) y (r:rs)
+                           where r = x `mod` y
+
+
+answer :: Int
+--answer = maximumBy (\x y -> compare (divCycleLength 1 x) (divCycleLength 1 y)) [1 .. 999]
+answer = fst $ maximumBy (\(_,x) (_,y) -> compare x y)
+                         $ [(x,divCycleLength 1 x) | x <- [1 .. 999]]
+
+main :: IO ()
+main = print answer
