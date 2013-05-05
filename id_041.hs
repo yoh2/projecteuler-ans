ae756389f88420e0600d1219a2669536cede2a8b
yoh2 <yoh2.sdj@gmail.com>
2013-05-06 07:49:58 +0900

    ID 41: Impremented faster permutation.
    Implemented function permutations' which lists permutations
    guaranteeing order without sort.
    On an interpreter, this runs slower than the previous version
    since permutations' will not be optimized, but runs faster
    if optimized by compiler.

--- id_041.hs
+++ id_041.hs
@@ -1,11 +1,24 @@
-import Data.List (permutations, sortBy, find)
-
 isPrime :: (Integral a) => a -> Bool
 isPrime x | x <= 1    = False
           | x == 2    = True
           | otherwise = and $ map ((/= 0) . (mod x))
                             $ takeWhile (\n -> n * n <= x)
                             $ 2:[3,5..]
+
+pickup1 :: [a] -> [(a, [a])]
+pickup1 []     = error "empty list."
+pickup1 [x]    = [(x, [])]
+pickup1 (x:xs) = (x, xs) : map (\ (y, ys) -> (y, x:ys))
+                                 (pickup1 xs)
+
+-- リストの要素の全ての並び順のパターンを得る。
+-- 元のリストの要素がが昇順または降順になっていた場合、
+-- 得られたリスト同士の要素の並び順も昇順または降順となる。
+permutations' :: [a] -> [[a]]
+permutations' [] = [[]]
+permutations' xs = concat
+                   $ map (\ (y, ys) -> map (y:) $ permutations' ys)
+                   $ pickup1 xs
 
 digitListToInt :: (Integral a) => [a] -> a
 digitListToInt = foldl1 (\x y-> x * 10 + y)
@@ -14,10 +27,9 @@ answer :: Integer
 answer = head
          $ filter isPrime
          $ concat
-         $ map (\xs -> sortBy (flip compare)
-                       $ map digitListToInt
-                       $ permutations xs)
-               [[1 .. n] | n <- [9, 8 .. 1]]
+         $ map (\xs -> map digitListToInt
+                       $ permutations' xs)
+               [[n, n - 1 .. 1] | n <- [9, 8 .. 1]]
 
 main :: IO ()
 main = print answer
