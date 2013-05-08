6c1d455bc6f024691906484b12cab96891bbabd1
yoh2 <yoh2.sdj@gmail.com>
2013-05-08 13:05:34 +0900

    ID 7: Updated algorithm to list primes.
    There is big change in the source, but basic idea for solution
    was not changed. Therefore I update the source instead of creating
    new one.

--- id_007.hs
+++ id_007.hs
@@ -1,18 +1,11 @@
-import Data.List (find)
+primes :: [Integer]
+primes = 2 : filter (\x -> and $ map ((/= 0) . (mod x))
+                               $ takeWhile (\y -> y * y <= x) primes)
+                    [3, 5 ..]
 
-nextCoprime :: (Integral a) => [a] -> (a -> a) -> a -> a
-nextCoprime [] _             y = y
--- xsが降順の時に速くなりやすい検索。
-nextCoprime xs next y =
-  if (find ((== 0) . (mod y)) $ reverse xs) == Nothing then y
-  else nextCoprime xs next (next y)
-
--- primeListList !! n = 小さい方からn個の素数が降順に並んだリスト
-primeListList :: [[Integer]]
-primeListList = []:[2]:(iterate (\xs -> (nextCoprime xs (2 + ) (2 + head xs)) : xs) [3, 2])
-
-solution :: Integer
-solution = head $ primeListList !! (10001)
+answer :: Integer
+answer = primes !! (n - 1)
+         where n = 10001
 
 main :: IO ()
-main = print solution
+main = print answer
